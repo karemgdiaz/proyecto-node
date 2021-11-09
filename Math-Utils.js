71f93240dc0c6dccbcdd46c378d7d8828aee7894
karemgdiaz <karemgdiaz@gmail.com>
2021-11-09 20:23:49 -0300

    commit practica

---
+++ Math-Utils.js
@@ -0,0 +1,21 @@
+function suma(a, b) {
+    console.log(a + b)
+    return a + b
+}
+function resta(a,b) {
+    console.log(a-b)
+    return a-b
+}
+function multiplicacion(a, b) {
+    console.log(a*b)
+    return a*b
+}
+function division(a, b) {
+    console.log(a/b)
+    return a/b
+}
+
+suma(10, 13)
+resta(43, 17)
+multiplicacion(12, 12)
+division(175, 15)
